2a83a18ccb5d913f416b38dd850805e8e388e86d
student <student@Win64-48.ue.loc>
2017-03-06 10:42:48 +0100

    init commit for app

---
+++ database/migrations/2017_03_06_091944_create_replies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('reply_id')->unsigned();
+            $table->foreign('reply_id')->references('id')->on('comments');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('replies')){
+            Schema::drop('replies');
+       }
+    }
+}
